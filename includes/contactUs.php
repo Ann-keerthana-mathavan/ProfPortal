80a62258715bd53db62edfaf87db5dcf0ebdca24
Shain922 <fmhd48763@gmail.com>
2023-11-25 12:12:50 +0530

    css codes in seperate files

---
+++ includes/contactUs.php
@@ -0,0 +1,103 @@
+ <!-- contact us start -->
+ <section class="contactUs" id="contactus">
+    <div class="container">
+      <div class="contact-text">
+        <p>contact information</p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <!-- contact info -->
+            <div class="contact-info">
+              <div class="row">
+                <div class="col">
+                  <!-- phone numbers -->
+                  <div class="row">
+                    <div class="col-1">
+                      <i class="fa fa-phone" aria-hidden="true"></i>
+                    </div>
+                    <div class="col">
+                      <p>
+                        +94 112 691 307 <br />
+                        +94 112 691 572
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <!-- address -->
+                  <div class="row">
+                    <div class="col-1">
+                      <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    </div>
+                    <div class="col">
+                      <p>
+                        320, T B jaya mawatha <br />
+                        colombo, sri lanka
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- social media icons -->
+              <div class="social-icons">
+                <a href="https://www.facebook.com/sliate.ac.lk?mibextid=ZbWKwL">
+                  <img src="./img/facebook icon.png" alt="" class="icons" />
+                </a>
+                <a href="https://www.linkedin.com/school/sliate/"><img src="./img/linkedin icon.png" alt="" class="icons" /></a>
+                <a href="https://www.youtube.com/@SLIATEOfficial">
+                  <img src="./img/youtube icon.png" class="icons" alt="" />
+                </a>
+                <a href="https://x.com/sliateofficial?t=P8lvrqh1D7MXnggfZ_jiOg&s=09">
+                  <img src="./img/X icon.png" class="icons" alt="" />
+                </a>
+              </div>
+              <div class="mapouter">
+                <div class="gmap_canvas">
+                  <iframe src="https://maps.google.com/maps?q=320,%20T%20B%20jaya%20mawatha%20%20colombo,%20sri%20lanka%20slaite&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 360px; height: 240px"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <!-- contact us form start -->
+            <div class="card contactUs-card">
+              <h3 class="text-center mt-3">contact us</h3>
+              <div class="card-body">
+                <form action="" method="POST" class="form">
+                  <!-- name -->
+                  <div class="form-group mt-2">
+                    <input type="text" class="form-control contactUs-input" name="name" id="name" placeholder="name" required />
+                  </div>
+                  <!-- email -->
+                  <div class="form-group mt-3">
+                    <input type="email" name="Email" class="form-control contactUs-input" placeholder="email" aria-describedby="emailHelp" required />
+                    <!-- message -->
+                    <div class="form-group mt-3">
+                      <textarea name="message" id="" cols="30" rows="4" class="form-control contactUs-input" placeholder="message" required></textarea>
+                    </div>
+                    <!-- button -->
+                    <div class="form-group">
+                      <button type="submit" class="btn btn-primary btn-block mt-3 d-block contactUs-btn">
+                        contact us
+                      </button>
+                    </div>
+                  </div>
+                  <!-- contact us form end -->
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="copy-rigth-text">
+        <p class="text-center mt-3">
+          <i class="fa fa-copyright" aria-hidden="true"></i> 2023 sliate all
+          rights reserved
+        </p>
+      </div>
+    </footer>
+  </section>
+ 
---
+++ includes/service.php
@@ -0,0 +1,58 @@
+<!-- service page start -->
+<section class="service pt-5 container" id="service">
+    <div class="container-fluid mt-5">
+        <div class="service-text">
+            <p>what can you do here</p>
+        </div>
+        <!-- service cards -->
+        <div class="container-fluid mt-5">
+            <div class="row mt-5 mb-5 d-flex align-items-stretch justify-content-start flex-grow-1">
+                <div class="col-lg-4">
+                    <div class="card mt-5 text-center service-card mx-5">
+                        <div class="card-img">
+                            <img class="img-fluid card-img" src="./img/manage timetable logo.png" alt="" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-heading">
+                                <p>manage time tables</p>
+                            </div>
+                            <p class="card-sub-text">
+                                you can view your time table and request empty time table
+                                here
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card mt-5 text-center service-card mx-5">
+                        <div class="card-img">
+                            <img class="img-fluid card-img" src="./img/manage logs logo.png" alt="" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-heading">
+                                <p>manage logs</p>
+                            </div>
+                            <p class="card-sub-text">you can manage subject logs here</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card mt-5 text-center service-card mx-5">
+                        <div class="card-img">
+                            <img class="img-fluid card-img" src="./img/manage vouchers logo.png" alt="" />
+                        </div>
+                        <div class="card-body">
+                            <div class="card-heading">
+                                <p>manage vouchers</p>
+                            </div>
+                            <p class="card-sub-text">
+                                you can manage vouchers and logs here
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- service page end -->
